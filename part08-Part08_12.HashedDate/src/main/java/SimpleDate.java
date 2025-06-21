4eb1e85922d73ae78646d36239985862ff3a8a64
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-21 13:57:18 +0700

    solve hashed date

--- part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
+++ part08-Part08_12.HashedDate/src/main/java/SimpleDate.java
@@ -34,6 +34,45 @@ public class SimpleDate {
 
         return false;
     }
+    
+//    @Override
+//    public int hashCode(){
+//        return this.year.hashCode();
+//    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 31 * hash + this.day;
+        hash = 31 * hash + this.month;
+        hash = 31 * hash + this.year;
+        return hash;
+    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (this == obj) {
+//            return true;
+//        }
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (getClass() != obj.getClass()) {
+//            return false;
+//        }
+//        final SimpleDate other = (SimpleDate) obj;
+//        if (this.day != other.day) {
+//            return false;
+//        }
+//        if (this.month != other.month) {
+//            return false;
+//        }
+//        if (this.year != other.year) {
+//            return false;
+//        }
+//        return true;
+//    }
+    
 
     public int differenceInYears(SimpleDate other) {
         if (earlier(other)) {
