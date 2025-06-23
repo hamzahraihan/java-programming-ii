fa5847dac70fe4f02ee85cab76f6123332fb6464
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-23 09:02:32 +0700

    solve tacoboxes

--- part09-Part09_05.TacoBoxes/src/main/java/Main.java
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -3,5 +3,15 @@ public class Main {
 
     public static void main(String[] args) {
         // Test your code here!
+        TripleTacoBox tripleTacos = new TripleTacoBox();
+        System.out.println(tripleTacos.tacosRemaining());
+        tripleTacos.eat();
+        tripleTacos.eat();
+        tripleTacos.eat();
+        tripleTacos.eat();
+        tripleTacos.eat();
+        tripleTacos.eat();
+        System.out.println(tripleTacos.tacosRemaining());
+
     }
 }
