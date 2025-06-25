073ed1d2fc30db74df04e202f5fa25660b389dfd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-25 13:53:42 +0700

    solve average of number

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -7,6 +7,16 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
+        ArrayList<Integer> inputs = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            inputs.add(Integer.valueOf(input));
+        }
+        double average = inputs.stream().mapToInt(value -> value).average().getAsDouble();
+        System.out.println("average of the numbers: " + average);
 
     }
 }
