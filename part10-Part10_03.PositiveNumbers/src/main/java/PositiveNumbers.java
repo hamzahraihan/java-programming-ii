15ed7f34b9472e285c37703c6bc059e1efcd9a23
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-25 13:53:59 +0700

    solve positive numbers

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,14 +1,27 @@
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PositiveNumbers {
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        List<Integer> numbers = new ArrayList<Integer>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            numbers.add(Integer.valueOf(input));
+        }
+        System.out.println(positive(numbers));
     }
-
+    
+    public static List<Integer> positive(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(num -> num > 0)
+                .collect(Collectors.toList());
+    }
+    
 }
