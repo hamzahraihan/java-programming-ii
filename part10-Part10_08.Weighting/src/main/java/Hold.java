6305318ebc943036e76beed64843a6a9ca5c0e3d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-26 13:27:17 +0700

    solve weighting

--- part10-Part10_08.Weighting/src/main/java/Hold.java
+++ part10-Part10_08.Weighting/src/main/java/Hold.java
@@ -1,5 +1,4 @@
 
-
 import java.util.ArrayList;
 
 public class Hold {
@@ -21,21 +20,27 @@ public class Hold {
     }
 
     public int totalWeight() {
-        int summa = 0;
-        int indeksi = 0;
-        while (indeksi < this.suitcases.size()) {
-        summa += this.suitcases.get(indeksi).totalWeight();
-        indeksi++;
-        }
-        return summa;
+//        int summa = 0;
+//        int indeksi = 0;
+//        while (indeksi < this.suitcases.size()) {
+//            summa += this.suitcases.get(indeksi).totalWeight();
+//            indeksi++;
+//        }
+//        return summa;
+
+        return this.suitcases.stream()
+                .mapToInt(value -> value.totalWeight())
+                .sum();
     }
 
     public void printItems() {
         int indeksi = 0;
         while (indeksi < this.suitcases.size()) {
-        this.suitcases.get(indeksi).printItems();
-        indeksi++;
+            this.suitcases.get(indeksi).printItems();
+            indeksi++;
         }
+        this.suitcases.stream()
+                .forEach(value -> System.out.println(value));
     }
 
     @Override
--- part10-Part10_08.Weighting/src/main/java/Suitcase.java
+++ part10-Part10_08.Weighting/src/main/java/Suitcase.java
@@ -2,42 +2,48 @@
 import java.util.ArrayList;
 
 public class Suitcase {
-
+    
     private int maximumWeight;
     private ArrayList<Item> Items;
-
+    
     public Suitcase(int maximumWeight) {
         this.Items = new ArrayList<>();
         this.maximumWeight = maximumWeight;
     }
-
+    
     public void addItem(Item Item) {
         if (this.totalWeight() + Item.getWeight() > this.maximumWeight) {
             return;
         }
-
+        
         this.Items.add(Item);
     }
-
+    
     public int totalWeight() {
-        int summa = 0;
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        summa += this.Items.get(indeksi).getweight();
-        indeksi++;
-        }
-        return summa;
-    }
+//        int summa = 0;
+//        int indeksi = 0;
+//        while (indeksi < this.Items.size()) {
+//            summa += this.Items.get(indeksi).getWeight();
+//            indeksi++;
+//        }
 
+        return this.Items.stream()
+                .mapToInt(value -> value.getWeight()).sum();
+        
+    }
+    
     public void printItems() {
-        int indeksi = 0;
-        while (indeksi < this.Items.size()) {
-        Item t = this.Items.get(indeksi);
-        System.out.println(t);
-        indeksi++;
-        }
+//        int indeksi = 0;
+//        while (indeksi < this.Items.size()) {
+//            Item t = this.Items.get(indeksi);
+//            System.out.println(t);
+//            indeksi++;
+//        }
+        this.Items.stream()
+                .map(value -> value)
+                .forEach(value -> System.out.println(value));
     }
-
+    
     public Item heaviestItem() {
         if (this.Items.isEmpty()) {
             return null;
@@ -46,17 +52,17 @@ public class Suitcase {
         // Tutustumme järjestämiseen hieman myöhemmin kurssilla
         return this.Items.stream().max((t1, t2) -> t1.getWeight() - t2.getWeight()).get();
     }
-
+    
     @Override
     public String toString() {
         if (this.Items.isEmpty()) {
             return "no items (0 kg)";
         }
-
+        
         if (this.Items.size() == 1) {
             return "1 item (" + this.totalWeight() + " kg)";
         }
-
+        
         return this.Items.size() + " items (" + this.totalWeight() + " kg)";
     }
 }
