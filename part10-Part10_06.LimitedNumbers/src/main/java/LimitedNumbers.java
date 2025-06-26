3d3400cf0b93b06bf0bddd15093992675d0dd052
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-26 13:27:08 +0700

    solve limited numbers

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,17 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.contains("-1")) {
+                break;
+            }
+            numbers.add(Integer.valueOf(input));
+        }
+        numbers.stream()
+                .map(value -> value)
+                .filter(value -> value >= 1 && value <= 5)
+                .forEach(value -> System.out.println(value));
     }
 }
