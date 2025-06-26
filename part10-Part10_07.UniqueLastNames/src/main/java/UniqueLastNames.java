02391e60570ed86e63af1bd60f24569766ce04a3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-26 13:27:13 +0700

    solve unique last names

--- part10-Part10_07.UniqueLastNames/src/main/java/UniqueLastNames.java
+++ part10-Part10_07.UniqueLastNames/src/main/java/UniqueLastNames.java
@@ -11,26 +11,41 @@ public class UniqueLastNames {
         while (true) {
             System.out.println("Continue personal information input? \"quit\" ends:");
             String continueQ = "quit";
-            if(scanner.hasNextLine()){continueQ = scanner.nextLine();}
+            if (scanner.hasNextLine()) {
+                continueQ = scanner.nextLine();
+            }
 
             if (continueQ.equals("quit")) {
                 break;
             }
 
-            System.out.print("Input first name: ");
+            System.out.println("Input first name: ");
             String firstName = "NaN";
-            if(scanner.hasNextLine()){firstName = scanner.nextLine();}
-            System.out.print("Input last name: ");
+            if (scanner.hasNextLine()) {
+                firstName = scanner.nextLine();
+            }
+
+            System.out.println("Input last name: ");
             String lastName = "NaN";
-            if(scanner.hasNextLine()){lastName = scanner.nextLine();}
-            System.out.print("Input the year of birth: ");
+            if (scanner.hasNextLine()) {
+                lastName = scanner.nextLine();
+            }
+
+            System.out.println("Input the year of birth: ");
             int birthYear = 0;
-            if(scanner.hasNextLine()){birthYear = Integer.valueOf(scanner.nextLine());}
+            if (scanner.hasNextLine()) {
+                birthYear = Integer.valueOf(scanner.nextLine());
+            }
 
             persons.add(new Person(firstName, lastName, birthYear));
             System.out.println("");
         }
 
         // Implement the printing of the unique last names in alphabetical order here:
+        persons.stream()
+                .map(value -> value.getLastName())
+                .distinct()
+                .sorted()
+                .forEach(value -> System.out.println(value));
     }
 }
