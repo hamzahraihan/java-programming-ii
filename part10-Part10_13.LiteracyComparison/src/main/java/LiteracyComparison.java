7b8b9b66064f5d9dd10673ccae38cd9654aab86e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-27 16:43:37 +0700

    solve literacy comparison

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,31 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LiteracyComparison {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        try {
+            List<String> dataCSV = Files.readAllLines(Paths.get("literacy.csv"));
+
+            dataCSV.sort((line1, line2) -> {
+                String[] partsA = line1.split(",");
+                String[] partsB = line2.split(",");
+
+                double rateA = Double.valueOf(partsA[5].trim());
+                double rateB = Double.valueOf(partsB[5].trim());
+
+                return Double.compare(rateA, rateB);
+            });
+
+            dataCSV.stream()
+                    .map(value -> value.split(","))
+                    .map(value -> value[3].trim() + " (" + Integer.valueOf(value[4].trim()) + "), " + value[2].trim().replace(" (%)", "") + ", " + Double.valueOf(value[5].trim()))
+                    .forEach(value -> System.out.println(value));
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
