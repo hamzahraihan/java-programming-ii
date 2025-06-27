46d4526ad7a88a16281a9ae294c975acb869aa6b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-27 16:43:22 +0700

    solve books from file

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -7,11 +7,25 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
+        String input = scanner.nextLine();
+        readBooks(input);
+    }
 
+    public static List<Book> readBooks(String file) {
+
+        try {
+            Files.lines(Paths.get(file))
+                    .map(value -> value.split(","))
+                    .map(parts -> new Book(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2]), parts[3]))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return new ArrayList<>();
     }
 
 }
