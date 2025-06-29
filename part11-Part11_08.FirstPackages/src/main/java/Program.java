5ca53a9818a9bbb4375b25afd952538a9451478f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-06-29 16:54:42 +0700

    solve first package

--- part11-Part11_08.FirstPackages/src/main/java/Program.java
+++ part11-Part11_08.FirstPackages/src/main/java/Program.java
@@ -1,10 +1,18 @@
-package mooc;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author Hamzah Raihan
+ */
 import mooc.logic.ApplicationLogic;
 import mooc.ui.UserInterface;
 import mooc.ui.TextInterface;
 
-public class Main {
+public class Program {
 
     public static void main(String[] args) {
         UserInterface ui = new TextInterface();
