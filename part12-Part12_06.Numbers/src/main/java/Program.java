feaad3c18029243f74315ca790ccda3e08e73a4d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-01 16:04:26 +0700

    solve numbers

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,13 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random randomize = new Random();
+        System.out.println("How many random numbers should be printed?");
+        int howMany = scanner.nextInt();
+        for (int i = 0; i < howMany; i++) {
+            int randomNumber = randomize.nextInt(11);
+            System.out.println(randomNumber);
+        }
     }
 
 }
