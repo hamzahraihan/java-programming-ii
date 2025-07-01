b52a615c18cdb169b8535c5912e15fffc66f8259
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-01 16:04:37 +0700

    solve array as a string

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -3,6 +3,24 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your method here
+        int[][] matrix = {
+            {3, 2, 7, 6},
+            {2, 4, 1, 0},
+            {3, 2, 1, 0}
+        };
+
+        System.out.println(arrayAsString(matrix));
     }
 
+    public static String arrayAsString(int[][] array) {
+        StringBuilder stringbuilder = new StringBuilder();
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++) {
+                int value = array[row][col];
+                stringbuilder.append(value);
+            }
+            stringbuilder.append("\n");
+        }
+        return stringbuilder.toString();
+    }
 }
