64b3a2f9afa78f34086f4022d408e8f3bf812534
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-02 12:26:26 +0700

    solve notifier

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,38 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
-public class NotifierApplication {
-
+public class NotifierApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        VBox vlayout = new VBox();
+        Button button = new Button("Update");
+        Label label = new Label();
+        TextField tf = new TextField();
+
+        button.setOnAction((event) -> {
+            label.setText(tf.getText());
+        });
+
+        vlayout.setSpacing(10);
+        vlayout.getChildren().addAll(tf, button, label);
+
+        Scene view = new Scene(vlayout);
+
+        window.setScene(view);
+        window.show();
+
     }
 
 }
