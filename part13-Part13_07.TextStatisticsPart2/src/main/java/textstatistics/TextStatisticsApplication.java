3c76af2d027c2ca23c8ac4c172490dd41c32c65b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-02 12:26:33 +0700

    solve text statistics part 2

--- part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,22 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
-public class TextStatisticsApplication {
-
+public class TextStatisticsApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(TextStatisticsApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        window.show();
     }
 
 }
