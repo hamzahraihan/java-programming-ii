c4bea12575274717e8b08afbee7c74e30bc2c1dc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-02 12:26:22 +0700

    solve text statistics

--- part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,38 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
 
-public class TextStatisticsApplication {
 
-
+public class TextStatisticsApplication extends Application {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(TextStatisticsApplication.class);
     }
-
+    
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        TextArea textArea = new TextArea("");
+        HBox horizontal = new HBox();
+        
+        layout.setCenter(textArea);
+        horizontal.setSpacing(10);
+        horizontal.getChildren().add(new Label("Letters: 0"));
+        horizontal.getChildren().add(new Label("Words: 0"));
+        horizontal.getChildren().add(new Label("The longest word is:"));
+        
+        layout.setBottom(horizontal);
+        
+        Scene view = new Scene(layout);
+        
+        window.setScene(view);
+        window.show();
+    }
+    
 }
