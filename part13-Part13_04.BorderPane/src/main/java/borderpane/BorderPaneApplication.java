14e15957087ae59cf63e78eaab817211113a6d14
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-02 12:26:18 +0700

    solve border pane

--- part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,32 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class BorderPaneApplication {
-
+public class BorderPaneApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(BorderPaneApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        Label north = new Label("NORTH");
+        Label south = new Label("SOUTH");
+        Label west = new Label("WEST");
+        Label east = new Label("EAST");
+        layout.setTop(north);
+        layout.setBottom(south);
+        layout.setLeft(west);
+        layout.setRight(east);
+
+        Scene view = new Scene(layout);
+        window.setScene(view);
+        window.show();
     }
 
 }
